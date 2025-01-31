b35a7a1330f5c7b4a49e507167c5151159e7b024
Vedant Anand <VEDANTANAND0077@gmail.com>
2025-01-31 22:40:34 +0530

    mobile video landing

--- app/Hero.jsx
+++ app/Hero.jsx
@@ -1,24 +1,32 @@
-import React from 'react'
+"use client";
+
+import React, { useState, useEffect } from 'react'
 
 function Hero() {
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768)
+    }
+
+    handleResize()
+
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  const videoSrc = isMobile 
+    ? 'https://res.cloudinary.com/dafbhjsmx/video/upload/v1737788891/graphene_animation0001-0200_1_ymfbfe.mp4' 
+    : 'https://res.cloudinary.com/dafbhjsmx/video/upload/v1736345819/VideoHero_hggmos.mp4'
+
   return (
     <div className='w-screen h-screen flex items-center justify-center bg-'>
       <div className='mx-4 my-4 rounded-lg flex items-center justify-center'>
-        <video src="https://res.cloudinary.com/dafbhjsmx/video/upload/v1736345819/VideoHero_hggmos.mp4" alt="Logo" className="object-contain rounded-2xl h-[32rem] lg:h-[60rem] md:h-[55rem] sm:h-[40rem] "
-        autoPlay
-        // loop
-        muted
-        ></video>
-        {/* <video
-          src="https://res.cloudinary.com/dafbhjsmx/video/upload/v1736345819/VideoHero_hggmos.mp4"
-          className="object-fill w-full h-full lg:h-[60rem] md:h-[55rem] sm:h-[40rem] mx-8 my-8 rounded-lg flex items-center justify-center border-2"
-          // controls
+        <video src={videoSrc} alt="Logo" className="object-contain rounded-2xl h-[32rem] lg:h-[60rem] md:h-[55rem] sm:h-[40rem]"
           autoPlay
           muted
-          loop
-        >
-          Your browser does not support the video tag.
-        </video> */}
+        ></video>
       </div>
     </div>
   )
